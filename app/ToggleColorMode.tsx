92a73c398c759518dbba3ac6de3b14e6046a3c09
Bamidele Barakat <bamideleaduke@gmail.com>
2023-05-22 11:32:57 +0100

    update

---
+++ app/ToggleColorMode.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import IconButton from "@mui/material/IconButton";
+import Box from "@mui/material/Box";
+import { useTheme, ThemeProvider, createTheme } from "@mui/material/styles";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import Navbar from "./components/NavBar";
+
+const ColorModeContext = React.createContext({
+  toggleColorMode: () => {},
+});
+export default function ToggleColorMode(children: any) {
+  const [mode, setMode] = React.useState<"light" | "dark">("light");
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        {/* <Navbar/> */}
+        {children}
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
--- app/context.tsx
+++ app/context.tsx
@@ -1,4 +1,69 @@
-"use client";
+// import React, { createContext, useState, useEffect, useContext } from "react";
+// import { createTheme, ThemeProvider, Theme } from "@mui/material/styles";
+
+// interface ColorModeContextType {
+//   toggleColorMode: () => void;
+//   mode: "light" | "dark";
+// }
+
+// const ColorModeContext = createContext<ColorModeContextType>({
+//   toggleColorMode: () => {},
+//   mode: "light",
+// });
+
+// export function useColorMode(): ColorModeContextType {
+//   return useContext(ColorModeContext);
+// }
+
+// export function ColorModeProvider({ children }: { children: React.ReactNode }) {
+//   const [mode, setMode] = useState<"light" | "dark">(() => {
+//     if (typeof window === "undefined") {
+//       return "light";
+//     }
+
+//     try {
+//       const storedMode = localStorage.getItem("themeMode");
+//       if (storedMode === "light" || storedMode === "dark") {
+//         return storedMode as "light" | "dark";
+//       }
+//     } catch (e) {
+//       console.error("Error reading from local storage:", e);
+//     }
+
+//     return "light";
+//   });
+
+//   useEffect(() => {
+//     try {
+//       localStorage.setItem("themeMode", mode);
+//     } catch (e) {
+//       console.error("Error writing to local storage:", e);
+//     }
+//   }, [mode]);
+
+//   const toggleColorMode = () => {
+//     setMode((prevMode) => {
+//       const newMode = prevMode === "light" ? "dark" : "light";
+//       localStorage.setItem("themeMode", newMode);
+//       console.log("Mode toggled:", newMode);
+//       return newMode;
+//     });
+//   };
+//   console.log("set mode ", mode);
+//   const theme: Theme = createTheme({
+//     palette: {
+//       mode: mode,
+//     },
+//   });
+
+//   return (
+//     <ColorModeContext.Provider value={{ toggleColorMode, mode }}>
+//       <ThemeProvider theme={theme}>{children}</ThemeProvider>
+//     </ColorModeContext.Provider>
+//   );
+// }
+
+// "use client";
 import React, { useContext, createContext } from "react";
 import { createTheme, ThemeProvider } from "@mui/material";
 import MyTheme from "./theme";
@@ -24,6 +89,7 @@ export const ColorModeProvider = ({ children }: any) => {
     try {
       const storedMode = localStorage.getItem("themeMode");
       if (storedMode === "light" || storedMode === "dark") {
+        console.log("stored mode from context", storedMode);
         return storedMode;
       }
     } catch (e) {
@@ -38,8 +104,12 @@ export const ColorModeProvider = ({ children }: any) => {
   //   }, [mode]);
 
   React.useEffect(() => {
+    if (!mode) {
+      return;
+    }
     try {
       localStorage.setItem("themeMode", mode);
+      console.log("theme mode from context", mode);
     } catch (e) {
       console.error("Error writing to local storage:", e);
     }
@@ -51,6 +121,7 @@ export const ColorModeProvider = ({ children }: any) => {
         setMode((prevMode) => {
           const newMode = prevMode === "light" ? "dark" : "light";
           localStorage.setItem("themeMode", newMode); // store the new mode in localStorage
+          console.log("new mode", newMode);
           return newMode;
         });
       },
@@ -65,41 +136,41 @@ export const ColorModeProvider = ({ children }: any) => {
   const theme = MyTheme(mode);
   const Theme = React.useMemo(
     () =>
-      //   createTheme({
-      //     palette: {
-      //       mode,
-      //       ...(mode === "light"
-      //         ? {
-      //             // palette: {
-      //             // palette: {
-      //             background: {
-      //               default: "#ffffff",
-      //               paper: "#f7f7f7",
-      //             },
+      createTheme({
+        palette: {
+          mode,
+          ...(mode === "light"
+            ? {
+                // palette: {
+                // palette: {
+                background: {
+                  default: "#ffffff",
+                  paper: "#f7f7f7",
+                },
 
-      //             primary: {
-      //               main: "#FF0000",
-      //             },
-      //             secondary: {
-      //               main: "#FFFF00",
-      //             },
-      //           }
-      //         : {
-      //             background: {
-      //               default: "#1c1c1c",
-      //               paper: "#252525",
-      //             },
-      //             primary: {
-      //               main: "#00FF00",
-      //             },
-      //             secondary: {
-      //               main: "#0000FF",
-      //             },
-      //           }),
-      //     },
-      //   }),
+                primary: {
+                  main: "#FF0000",
+                },
+                secondary: {
+                  main: "#FFFF00",
+                },
+              }
+            : {
+                background: {
+                  default: "#1c1c1c",
+                  paper: "#252525",
+                },
+                primary: {
+                  main: "#00FF00",
+                },
+                secondary: {
+                  main: "#0000FF",
+                },
+              }),
+        },
+      }),
 
-      theme,
+    // theme,
     [theme]
   );
   return (
